5ac06e745efa50633a3d31372b0ebe238efac621
Zach Tang <zachtangoj@gmail.com>
2023-07-20 19:07:02 -0700

    search tree end of chapter questions finished

---
+++ ch11_search_tree/end_of_chapter.cpp
@@ -0,0 +1,16 @@
+/*
+
+    These questions are NOT GPT generated, they were provided at the end of the chapter
+
+    Problem 11.1 (S) Which of the following statements are true?
+        a. The height of a binary search tree with n nodes cannot be
+        smaller than O(log n).
+        b. All the operations supported by a binary search tree (except
+        OutputSorted) run in O(log n) time.
+    Problem 11.2 You are given a binary tree with n nodes (via a
+    pointer to its root). Each node of the tree has a size field, as in
+    Section 11.3.9, but these fields have not been filled in yet. How much
+    time is necessary and sufficient to compute the correct value for all
+    the size fields?
+        b. O(n)
+*/
