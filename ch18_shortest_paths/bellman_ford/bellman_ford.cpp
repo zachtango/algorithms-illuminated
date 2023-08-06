971d7746718fd7139d7ab1f28cb45e6180ccaf79
Zach Tang <zachtangoj@gmail.com>
2023-08-05 17:40:50 -0700

    reimplement bellman ford with adjacency lists and tests

--- ch18_shortest_paths/bellman_ford/bellman_ford.cpp
+++ ch18_shortest_paths/bellman_ford/bellman_ford.cpp
@@ -2,44 +2,130 @@
 using std::cout;
 #include <vector>
 using std::vector;
+#include <stdexcept>
+using std::runtime_error;
 #include <algorithm>
 using std::min;
 
-typedef vector<vector<int>> am;
+typedef vector<vector<vector<int>>> al;
 
-vector<vector<int>> bellman_ford(const am &graph, int s) {
+#define INF 10000000
+
+// Graph is adjacency list (directed graph)
+vector<vector<int>> bellmanFord(const al &graph, int s) {
     int n = graph.size();
 
-    /*
-        Case 1: Li - 1, v
-        Case 2: Li - 1, k + weight(k, v)
-    */
-    vector<vector<int>> L(n + 1, vector<int>(n + 1));
-    
-    // Base case
+    // Store edges (w, v) for all v to get access to all (w, v) for v in the algo
+    al revGraph(n);
+
     for (int v = 0; v < n; v++) {
-        L[0][v] = graph[s][v];
+        for (auto &edge : graph[v]) {
+            int neighbor = edge[0],
+                weight = edge[1];
+
+            revGraph[neighbor].push_back({v, weight});
+        }
     }
 
-    // Recurrence
+    // Store length of shortest paths
+    vector<vector<int>> L(n + 1, vector<int>(n));
+
+    // Set base cases
+    for (int neighbor = 0; neighbor < n; neighbor++) {
+        L[0][neighbor] = INF;
+    }
+    for (auto &edge : graph[s]) {
+        int neighbor = edge[0],
+            weight = edge[1];
+        
+        L[0][neighbor] = weight;
+    }
+    L[0][s] = 0;
+
+    // Bellman fords
     for (int i = 1; i <= n; i++) {
-        for(int v = 0;  v < n; v++) {
+        for (int v = 0; v < n; v++) {
             L[i][v] = L[i - 1][v];
 
-            for (int k = 0; k < n; k++) {
-                L[i][v] = min(L[i][v], L[i - 1][k] + graph[k][v]);
+            for (auto &edge : revGraph[v]) {
+                int neighbor = edge[0],
+                    weight = edge[1];
+                
+                if (L[i - 1][neighbor] == INF) {
+                    continue;
+                }
+
+                L[i][v] = min(L[i][v], L[i - 1][neighbor] + weight);
             }
+        }
+    }
+    
+    // Negative cycle check
+    // Check if Ln,v == Ln - 1,v for all v
+    for (int v = 0; v < n; v++) {
+        if (L[n][v] != L[n - 1][v]) {
+            throw runtime_error("Negative Cycle in Graph");
         }
     }
 
     return L;
 }
 
+void printShortestDistances(const std::vector<std::vector<int>>& result) {
+    for (int i = 0; i < result.size() - 1; i++) {
+        std::cout << "Step " << i << ":\n";
+        for (int v = 0; v < result[i].size(); v++) {
+            if (result[i][v] == INF) {
+                std::cout << "To node " << v << ": " << "INF\n";
+            } else {
+                std::cout << "To node " << v << ": " << result[i][v] << "\n";
+            }
+        }
+        std::cout << "\n";
+    }
+}
 
 int main() {
-    
+    // Test Case 1: Simple graph with 3 nodes
+    al graph1 = {
+        { {{1, 3}} },
+        { {{2, 2}} },
+        { {{0, -4}}, {{1, 1}} }
+    };
+    int start_node1 = 0;
+    std::cout << "Test Case 1: Simple graph with 3 nodes\n";
+    std::vector<std::vector<int>> result1 = bellmanFord(graph1, start_node1);
+    printShortestDistances(result1);
+    std::cout << "\n";
 
+    // Test Case 2: Graph with a negative cycle
+    al graph2 = {
+        { {{1, 1}} },
+        { {{2, 2}} },
+        { {{0, -6}}, {{1, -3}} }
+    };
+    int start_node2 = 0;
+    std::cout << "Test Case 2: Graph with a negative cycle\n";
+    try {
+        std::vector<std::vector<int>> result2 = bellmanFord(graph2, start_node2);
+        printShortestDistances(result2);
+    } catch (const std::runtime_error& e) {
+        std::cout << "Error: " << e.what() << "\n";
+    }
+    std::cout << "\n";
+
+    // Test Case 3: Large graph
+    al graph3 = {
+        { {{1, 3}}, {{2, 5}}, {{3, 2}} },
+        { {{2, 1}} },
+        { {{3, 6}} },
+        { }
+    };
+    int start_node3 = 0;
+    std::cout << "Test Case 3: Large graph\n";
+    std::vector<std::vector<int>> result3 = bellmanFord(graph3, start_node3);
+    printShortestDistances(result3);
+    std::cout << "\n";
 
     return 0;
 }
-
